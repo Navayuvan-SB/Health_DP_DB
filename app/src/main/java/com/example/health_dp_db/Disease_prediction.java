2a42ca0f61903a63e0a18617cadaad4c2f7d5347
Navayuvan-NS <ns.navayuvan@gmail.com>
2019-02-25 01:51:23 +0530

    25.02.2019

--- app/src/main/java/com/example/health_dp_db/Disease_prediction.java
+++ app/src/main/java/com/example/health_dp_db/Disease_prediction.java
@@ -2,6 +2,7 @@ package com.example.health_dp_db;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -31,11 +32,12 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Disease_prediction extends Fragment {
 
     private AutoCompleteTextView autocompleteText;
-    
+
     private TextView sympLabel;
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
@@ -43,6 +45,12 @@ public class Disease_prediction extends Fragment {
     private DatabaseReference mRootReference = firebaseDatabase.getReference();
 
     private DatabaseReference mSymptomsReference = mRootReference.child("Symptoms");
+
+    private DatabaseReference mDiseaseReference = mRootReference.child("Disease");
+
+    private DatabaseReference mMatchReference = mRootReference.child("Match");
+
+    private DatabaseReference mResultReference = mRootReference.child("Result");
 
     private ImageView plusBtn;
 
@@ -56,6 +64,16 @@ public class Disease_prediction extends Fragment {
 
     private ArrayList<String> contents;
 
+    private ArrayList<Integer> sympIds = new ArrayList<Integer>();
+
+    private static int maxIds = 0;
+
+    public String predDiseaseId ;
+
+    private TextView resultTextView;
+
+    private ArrayList<Integer> resInInt = new ArrayList<Integer>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -68,7 +86,8 @@ public class Disease_prediction extends Fragment {
 
         sympLabel = view.findViewById(R.id.SymptomsLabel);
 
-        predictBtn = (Button) view.findViewById(R.id.predictBtn);
+        predictBtn = view.findViewById(R.id.predictBtn);
+
 
         contents = new ArrayList<String>();
 
@@ -94,9 +113,9 @@ public class Disease_prediction extends Fragment {
 
         autocompleteText.setAdapter(adapter);
 
-        plusBtn = (ImageView) view.findViewById(R.id.plusBtnImg);
+        plusBtn = view.findViewById(R.id.plusBtnImg);
 
-        selectedSympList = (ListView) view.findViewById(R.id.selectedSymp);
+        selectedSympList = view.findViewById(R.id.selectedSymp);
 
         selectedSymp = new ArrayList<String>();
 
@@ -142,16 +161,126 @@ public class Disease_prediction extends Fragment {
             @Override
             public void onClick(View v) {
                 predition();
+
             }
         });
 
         return view;
     }
 
+
     private void predition() {
 
+            mSymptomsReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (String str : selectedSymp){
+                        sympIds.add(Integer.parseInt(dataSnapshot.child(str).getValue().toString()));
+                        //System.out.println(Integer.parseInt(dataSnapshot.child(str).getValue().toString()));
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+            mMatchReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                    for (DataSnapshot childDataSnapshot : dataSnapshot.getChildren()){
+
+                        int count = 0;
+                        String get = childDataSnapshot.getValue().toString();
+
+                        StringTokenizer tokenizer = new StringTokenizer(get, ", ");
+                        while (tokenizer.hasMoreTokens()) {
+
+                            int j = Integer.parseInt(tokenizer.nextToken());
+
+                            System.out.println(j);
+
+                            for (int i : sympIds){
+                              if (i == j){
+
+                                  count += 1;
+                                  System.out.print("True ");
+                                  System.out.print(i);
+                                  System.out.print(" : ");
+                                  System.out.print(j);
+                                  System.out.print("\n");
+                              }
+                            }
+                            //System.out.println(tokenizer.nextToken());
+                        }
+
+                        if (count > maxIds){
+
+                            predDiseaseId = childDataSnapshot.getKey();
+
+                           // System.out.print("Whhhoooo");
+                           // System.out.print(predDiseaseId);
+
+                            maxIds = count;
+
+                            //System.out.println("Changed");
+                        }
+
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+
+            });
+
+        mResultReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String str = dataSnapshot.getValue().toString();
+
+                int res = Integer.parseInt(str);
+
+                resInInt.add(res);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        mDiseaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                int i = resInInt.get(0);
+
+                for (DataSnapshot child : dataSnapshot.getChildren()){
+                    if (Integer.parseInt(child.getValue().toString()) == i){
+                        Toast.makeText(getActivity().getApplicationContext(), child.getKey(), Toast.LENGTH_LONG).show();
+                        System.out.println("\n\n\n Result \n\n\n");
+                        System.out.println(child.getKey());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
+
+
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
